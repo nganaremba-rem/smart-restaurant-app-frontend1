c634c333fa0fd8a22e298e889a117f9da7c584a5
Nganaremba <nganaremba-rem>
2024-04-23 21:49:59 +0530

    fixed some bad code

--- src/App.js
+++ src/App.js
@@ -1,120 +1,120 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState, useEffect, useContext } from "react";
+import { useContext, useEffect } from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
+import UnauthorizedPage from "./UnautorizedPage";
+import ViewCart from "./cart/ViewCart";
+import { CartProvider } from "./context/Cart";
+import { SocketContext } from "./context/socket";
+import MenuList from "./menu/MenuList";
+import Checkout from "./order/Checkout";
+import PastOrders from "./order/PastOrders";
+import ViewOrders from "./order/ViewOrders";
 import SignIn from "./signIn/SignIn";
 import SignUp from "./signup/SignUp";
 import VerifyOTP from "./signup/VerifyOTP";
-import { CartProvider } from "./context/Cart";
-import MenuList from "./menu/MenuList";
-import ViewCart from "./cart/ViewCart";
-import ViewOrders from "./order/ViewOrders";
-import Checkout from "./order/Checkout";
-import PastOrders from "./order/PastOrders";
-import UnauthorizedPage from "./UnautorizedPage";
-import { SocketContext } from "./context/socket";
 
 function App() {
-  const socket = useContext(SocketContext);
-  const user = JSON.parse(localStorage.getItem("SRA_userData"));
-  let role = "";
-  if (user) {
-    role = user.role;
-  }
-  useEffect(() => {
-    socket.on("pick_order", (data) => {
-      console.log("pick_order");
-      toast.info(data, {
-        position: "bottom-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        progress: undefined,
-        theme: "light",
-      });
-    });
-    socket.on("chef_ended", (data) => {
-      toast.info(data, {
-        position: "bottom-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        progress: undefined,
-        theme: "light",
-      });
-    });
-    socket.on("waiter_confirmed", (data) => {
-      toast.info(data, {
-        position: "bottom-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        progress: undefined,
-        theme: "light",
-      });
-    });
-    socket.on("chef_started", (data) => {
-      toast.info(data, {
-        position: "bottom-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        progress: undefined,
-        theme: "light",
-      });
-    });
+	const socket = useContext(SocketContext);
+	const user = JSON.parse(localStorage.getItem("SRA_userData"));
+	let role = "";
+	if (user) {
+		role = user.role;
+	}
+	useEffect(() => {
+		socket.on("pick_order", (data) => {
+			console.log("pick_order");
+			toast.info(data, {
+				position: "bottom-right",
+				autoClose: 5000,
+				hideProgressBar: false,
+				closeOnClick: true,
+				progress: undefined,
+				theme: "light",
+			});
+		});
+		socket.on("chef_ended", (data) => {
+			toast.info(data, {
+				position: "bottom-right",
+				autoClose: 5000,
+				hideProgressBar: false,
+				closeOnClick: true,
+				progress: undefined,
+				theme: "light",
+			});
+		});
+		socket.on("waiter_confirmed", (data) => {
+			toast.info(data, {
+				position: "bottom-right",
+				autoClose: 5000,
+				hideProgressBar: false,
+				closeOnClick: true,
+				progress: undefined,
+				theme: "light",
+			});
+		});
+		socket.on("chef_started", (data) => {
+			toast.info(data, {
+				position: "bottom-right",
+				autoClose: 5000,
+				hideProgressBar: false,
+				closeOnClick: true,
+				progress: undefined,
+				theme: "light",
+			});
+		});
 
-    if (role === "waiter") {
-      socket.emit("join_waiters_room", { waiter: `${user._id}` });
-    }
-    if (role === "chef") {
-      socket.emit("join_chefs_room", { chef: `${user._id}` });
-    }
-    if (role === "customer") {
-      socket.emit("join_customer_room", {
-        customer: user._id,
-      });
-    }
-  }, [socket]);
-  return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/checkout" element={<Checkout />}></Route>
-          <Route path="/signin" element={<SignIn />}></Route>
-          <Route path="/" element={<SignIn />}></Route>
-          <Route path="/signup" element={<SignUp />}></Route>
-          <Route path="/verify-otp" element={<VerifyOTP />}></Route>
-          <Route
-            path="/menu"
-            element={
-              <CartProvider>
-                <MenuList />
-              </CartProvider>
-            }
-          ></Route>
-          <Route
-            path="/cart"
-            element={
-              <CartProvider>
-                <ViewCart />
-              </CartProvider>
-            }
-          ></Route>
-          <Route
-            path="/orders"
-            element={
-              <CartProvider>
-                <ViewOrders />
-              </CartProvider>
-            }
-          ></Route>
-          <Route path="/past-orders" element={<PastOrders />}></Route>
-          <Route path="/unauthorized" element={<UnauthorizedPage />} />
-        </Routes>
-      </Router>
-      <ToastContainer />
-    </>
-  );
+		if (role === "waiter") {
+			socket.emit("join_waiters_room", { waiter: `${user._id}` });
+		}
+		if (role === "chef") {
+			socket.emit("join_chefs_room", { chef: `${user._id}` });
+		}
+		if (role === "customer") {
+			socket.emit("join_customer_room", {
+				customer: user._id,
+			});
+		}
+	}, [socket, role, user]);
+	return (
+		<>
+			<Router>
+				<Routes>
+					<Route path="/checkout" element={<Checkout />} />
+					<Route path="/signin" element={<SignIn />} />
+					<Route path="/" element={<SignIn />} />
+					<Route path="/signup" element={<SignUp />} />
+					<Route path="/verify-otp" element={<VerifyOTP />} />
+					<Route
+						path="/menu"
+						element={
+							<CartProvider>
+								<MenuList />
+							</CartProvider>
+						}
+					/>
+					<Route
+						path="/cart"
+						element={
+							<CartProvider>
+								<ViewCart />
+							</CartProvider>
+						}
+					/>
+					<Route
+						path="/orders"
+						element={
+							<CartProvider>
+								<ViewOrders />
+							</CartProvider>
+						}
+					/>
+					<Route path="/past-orders" element={<PastOrders />} />
+					<Route path="/unauthorized" element={<UnauthorizedPage />} />
+				</Routes>
+			</Router>
+			<ToastContainer />
+		</>
+	);
 }
 
 export default App;
--- src/signup/SignUp.js
+++ src/signup/SignUp.js
@@ -1,207 +1,207 @@
-import * as React from "react";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Link from "@mui/material/Link";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import axios from "axios";
+import * as React from "react";
+import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useNavigate } from "react-router-dom";
 import Config from "../config/Config";
 // TODO remove, this demo shouldn't need to reset the theme.
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#000000",
-    },
-  },
-  typography: {
-    fontFamily: ["Amaranth", "sans-serif"].join(","),
-  },
+	palette: {
+		primary: {
+			main: "#000000",
+		},
+	},
+	typography: {
+		fontFamily: ["Amaranth", "sans-serif"].join(","),
+	},
 });
 
 export default function SignUp() {
-  const navigate = useNavigate();
-  React.useEffect(() => {
-    const user = JSON.parse(localStorage.getItem("SRA_userData"));
-    if (user) {
-      const token = user.token;
-      if (
-        token &&
-        Math.floor(Date.now() / 1000) <
-          JSON.parse(atob(token.split(".")[1])).exp
-      ) {
-        if (user.role === "customer") {
-          navigate("/menu");
-        } else {
-          navigate("/orders");
-        }
-      }
-    }
-  }, []);
-  async function handlePost(newUser) {
-    try {
-      console.log("post user" + newUser);
-      const { data } = await axios.post(
-        `${Config.API_BASE_URL}users/signup`,
-        newUser
-      );
-      console.log("post data" + data);
-      if (data) {
-        toast.success(`OTP sent to ${data.email}`, {
-          position: "bottom-right",
-          autoClose: 3000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          progress: undefined,
-          theme: "light",
-        });
-        localStorage.setItem("SRA_userData", JSON.stringify(data));
-      }
-      navigate("/verify-otp");
-    } catch (err) {
-      toast.error(`${err.response.data.message}`, {
-        position: "bottom-right",
-        autoClose: 3000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        progress: undefined,
-        theme: "light",
-      });
-    }
-  }
+	const navigate = useNavigate();
+	React.useEffect(() => {
+		const user = JSON.parse(localStorage.getItem("SRA_userData"));
+		if (user) {
+			const token = user.token;
+			if (
+				token &&
+				Math.floor(Date.now() / 1000) <
+					JSON.parse(atob(token.split(".")[1])).exp
+			) {
+				if (user.role === "customer") {
+					navigate("/menu");
+				} else {
+					navigate("/orders");
+				}
+			}
+		}
+	}, [navigate]);
+	async function handlePost(newUser) {
+		try {
+			console.log(`post user ${newUser}`);
+			const { data } = await axios.post(
+				`${Config.API_BASE_URL}users/signup`,
+				newUser,
+			);
+			console.log(`post data${data}`);
+			if (data) {
+				toast.success(`OTP sent to ${data.email}`, {
+					position: "bottom-right",
+					autoClose: 3000,
+					hideProgressBar: false,
+					closeOnClick: true,
+					progress: undefined,
+					theme: "light",
+				});
+				localStorage.setItem("SRA_userData", JSON.stringify(data));
+			}
+			navigate("/verify-otp");
+		} catch (err) {
+			toast.error(`${err.response.data.message}`, {
+				position: "bottom-right",
+				autoClose: 3000,
+				hideProgressBar: false,
+				closeOnClick: true,
+				progress: undefined,
+				theme: "light",
+			});
+		}
+	}
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log("submit data" + data);
-    const passwordsMatch =
-      data.get("password") === data.get("confirm-password");
-    if (!passwordsMatch) {
-      toast.warn("passwords do not match", {
-        position: "bottom-right",
-        autoClose: 3000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        progress: undefined,
-        theme: "light",
-      });
-    } else {
-      let newUser = {
-        firstName: data.get("firstName"),
-        lastName: data.get("lastName"),
-        email: data.get("email"),
-        password: data.get("password"),
-        role: "customer",
-      };
-      console.log(newUser);
-      handlePost(newUser);
-    }
-  };
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		const data = new FormData(event.currentTarget);
+		console.log(`submit data: ${data}`);
+		const passwordsMatch =
+			data.get("password") === data.get("confirm-password");
+		if (!passwordsMatch) {
+			toast.warn("passwords do not match", {
+				position: "bottom-right",
+				autoClose: 3000,
+				hideProgressBar: false,
+				closeOnClick: true,
+				progress: undefined,
+				theme: "light",
+			});
+		} else {
+			const newUser = {
+				firstName: data.get("firstName"),
+				lastName: data.get("lastName"),
+				email: data.get("email"),
+				password: data.get("password"),
+				role: "customer",
+			};
+			console.log(newUser);
+			handlePost(newUser);
+		}
+	};
 
-  return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 3,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <img
-            src="cartoon.png"
-            style={{ height: 90, width: 90 }}
-            alt="not found"
-          ></img>
+	return (
+		<ThemeProvider theme={theme}>
+			<Container component="main" maxWidth="xs">
+				<CssBaseline />
+				<Box
+					sx={{
+						marginTop: 3,
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+					}}
+				>
+					<img
+						src="cartoon.png"
+						style={{ height: 90, width: 90 }}
+						alt="not found"
+					/>
 
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
-          <Box
-            component="form"
-            onSubmit={(e) => handleSubmit(e)}
-            sx={{ mt: 3 }}
-          >
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  name="lastName"
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  type="email"
-                  label="Email"
-                  name="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  inputProps={{ minLength: 8 }}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="confirm-password"
-                  label="Confirm Password"
-                  type="password"
-                  id="confirm-password"
-                  inputProps={{ minLength: 8 }}
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 2, mb: 2 }}
-              style={{ backgroundColor: "#ff841c", color: "white" }}
-            >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="center">
-              <Grid item>
-                <Link href="/signin" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-      </Container>
-    </ThemeProvider>
-  );
+					<Typography component="h1" variant="h5">
+						Sign up
+					</Typography>
+					<Box
+						component="form"
+						onSubmit={(e) => handleSubmit(e)}
+						sx={{ mt: 3 }}
+					>
+						<Grid container spacing={2}>
+							<Grid item xs={12}>
+								<TextField
+									name="firstName"
+									required
+									fullWidth
+									id="firstName"
+									label="First Name"
+									autoFocus
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									name="lastName"
+									required
+									fullWidth
+									id="lastName"
+									label="Last Name"
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									required
+									fullWidth
+									id="email"
+									type="email"
+									label="Email"
+									name="email"
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									required
+									fullWidth
+									name="password"
+									label="Password"
+									type="password"
+									id="password"
+									inputProps={{ minLength: 8 }}
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									required
+									fullWidth
+									name="confirm-password"
+									label="Confirm Password"
+									type="password"
+									id="confirm-password"
+									inputProps={{ minLength: 8 }}
+								/>
+							</Grid>
+						</Grid>
+						<Button
+							type="submit"
+							fullWidth
+							variant="contained"
+							sx={{ mt: 2, mb: 2 }}
+							style={{ backgroundColor: "#ff841c", color: "white" }}
+						>
+							Sign Up
+						</Button>
+						<Grid container justifyContent="center">
+							<Grid item>
+								<Link href="/signin" variant="body2">
+									Already have an account? Sign in
+								</Link>
+							</Grid>
+						</Grid>
+					</Box>
+				</Box>
+			</Container>
+		</ThemeProvider>
+	);
 }
