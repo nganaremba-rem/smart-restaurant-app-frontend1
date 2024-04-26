9417496da4573362fcf3b61a3e95e5651c668ff4
Nganaremba <nganaremba-rem>
2024-04-26 13:28:57 +0530

    changed api endpoint

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,5 +1,6 @@
 const Config = {
-  API_BASE_URL: "http://localhost:5000/restaurant/api/v1/",
+	API_BASE_URL:
+		"https://smart-restaurant-app-backend.vercel.app/restaurant/api/v1/",
 };
 
 export default Config;
--- src/context/socket.js
+++ src/context/socket.js
@@ -1,13 +1,13 @@
-import io from "socket.io-client";
 import React from "react";
+import io from "socket.io-client";
 import Config from "../config/Config";
 let x = Config.API_BASE_URL;
 
-let connectionString  = "http://10.250.1.216:5000";
-if(x.includes("localhost")){
-  connectionString = "http://localhost:5000";
+let connectionString = "https://smart-restaurant-app-backend.vercel.app";
+if (x.includes("smart-restaurant-app-backend.vercel.app")) {
+	connectionString = "https://smart-restaurant-app-backend.vercel.app";
 }
 export const socket = io.connect(`${connectionString}`, {
-  transports: ["websocket"],
+	transports: ["websocket"],
 });
 export const SocketContext = React.createContext();
