1f5139b44a413ec393bf98f30f1d0aa9248c788b
Nganaremba <nganaremba-rem>
2024-04-30 20:22:55 +0530

    added / for the socketjs web socket connection string

--- src/context/socket.js
+++ src/context/socket.js
@@ -1,12 +1,13 @@
 import React from "react";
 import io from "socket.io-client";
 import Config from "../config/Config";
-let x = Config.API_BASE_URL;
+const x = Config.API_BASE_URL;
 
-let connectionString = "https://smart-restaurant-app-backend.vercel.app";
+let connectionString = "https://smart-restaurant-app-backend.vercel.app/";
 if (x.includes("smart-restaurant-app-backend.vercel.app")) {
-	connectionString = "https://smart-restaurant-app-backend.vercel.app";
+	connectionString = "https://smart-restaurant-app-backend.vercel.app/";
 }
+
 export const socket = io.connect(`${connectionString}`, {
 	transports: ["websocket"],
 });
